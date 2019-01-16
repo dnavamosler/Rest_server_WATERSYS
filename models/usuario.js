48eb58d3f79c24dabca39547f2b82e9819c08082
daniel nava <dnavamoslercl@gmail.com>
2019-01-16 11:15:42 -0300

    esquema de usuario realizado

--- models/usuario.js
+++ models/usuario.js
@@ -8,14 +8,32 @@ let Schema = mongoose.Schema
 /* instancia de esquema 
     para usuarios */
 let usuarioSchema = new Schema({
-    nombre:{
-        type: String,
-        required: [true,'El nombre de usuario es requerido'] 
-    },
-    apellido:{
-        type: String,
-        required: [true, 'El apellido de usuario es requerido']
-    }
+    name : [{                                    /* Nombre y apellido del usuario */
+        first_name : 
+            { type: String, required: true},
+        last_name : 
+        {type: String, required: true}
+    }],
+    address : [{                                   /* Direccion del usuario */
+        city : 
+            {type : String, required: true},
+        street: 
+            {type: String, required: true},
+        number : 
+            {type : Number, required: true}
+    }],
+    plan_id :                                       /* id, del plan que va a tener */
+        {type : Schema.Types.ObjectId, required: true},
+    meses_deuda : [{                                /* deuda por defecto en 0 */
+        cant_meses : {type: Number, default: 0},
+        meses : {type : Array, default: 0}
+    }],     
+    bidones : [{                                    /* Bidones cantidad y deuda que acumula, hace falta un valor constante para el precio */
+        cant_bidones:{type: Number, default: 0},
+        deuda: {type: Number, default: 0}
+    }],
+    email: {type: String, required: false},         /* Correo electronico */
+    state: {type: Boolean, default : true}            /* estado activo, si cancela el plan y su deuda total es 0, pasa a falso y no se muestra mas. */
 })
 
 module.exports = mongoose.model('Usuario', usuarioSchema)
