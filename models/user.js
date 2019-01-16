f0f818753657b7a00d7190f528720eb605ad6526
daniel nava <dnavamoslercl@gmail.com>
2019-01-16 12:33:23 -0300

    user post lsto

---
+++ models/user.js
@@ -0,0 +1,39 @@
+/*  Dependencias */
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+/* creacion de esquemas con mongoose */
+let Schema = mongoose.Schema
+
+/* Roles validos */
+let rolesValidos = {
+    values: ['ADMIN_ROL','USER_ROL'],
+    message : '{VALUE}, no es un rol valido.'
+}
+
+/* instancia de esquema 
+    para users */
+let userSchema = new Schema({
+    name : [{                                    /* Nombre y apellido del user */
+        first_name : 
+            { type: String, required: true},
+        last_name : 
+        {type: String, required: true}
+    }],
+    phone: {type: String, required: true}, /* numero de telefono */
+    nickname: {type: String, required: true, unique: true}, /* nickname unico */
+    password : {type: String, required: true}, /* password encriptado */
+    rol : { type: String, default : 'USER_ROL', enum : rolesValidos}    /* rol en la palicacion */,
+    state: {type: Boolean, default : true}            /* estado activo, si cancela el plan y su deuda total es 0, pasa a falso y no se muestra mas. */
+})
+
+userSchema.plugin( uniqueValidator , { message : '{PATH} debe ser unico.'})
+
+usuarioSchema.methods.toJSON = function(){
+    let user = this         /* obtiene el this actual */
+    let userObject = user.toObject()        /* declara el esquema como objeto */
+    delete userObject.password      /* elimina el password */
+    return userObject
+}
+
+module.exports = mongoose.model('User', userSchema)
---
+++ routes/user.js
@@ -0,0 +1,68 @@
+/* Importacion de dependencias */
+
+const express = require('express')
+const User = require('../models/client')
+const bcrypt = require('bcryptjs');
+
+/* instancia de express */
+const app = express()
+
+
+
+
+/* metodo get para obtencion de users */
+// app.get('/user', function(req, res) {
+//     res.json('get user')
+// })
+
+/* metodo post para creacion de users */
+app.post('/user', function(req, res) {
+
+    let body = req.body
+    
+    /* crear el modelo del user */
+    let user = new User({
+        name:{
+            first_name : body.first_name,
+            last_name : body.last_name},
+        phone : body.phone,
+        nickname : body.nickname,
+        password : bcrypt.hashSync(body.password , 10 ) /* Codifica el password */     
+    })
+
+    user.save((err, userDB) => {
+        if(err) {
+            /* retornar un error en caso de que el request sea erroneo */
+            return res.status(400).json({
+                ok : false,
+                err
+            })
+        }
+
+        /* responder el request */
+        res.json({
+            ok: true,
+            user : userDB
+        })
+    })
+
+})
+
+/* metodo put para actualizacion de users */
+
+ app.put('/user', function(req, res) {
+
+    // let id = req.params.id esto es para pasar parametros por id
+
+    res.json({ id })
+}) 
+
+
+/* metodo delete para borrado de users */
+/* app.delete('/user', function(req, res) {
+
+    res.send('delete user')
+})
+ */
+
+ module.exports = app
