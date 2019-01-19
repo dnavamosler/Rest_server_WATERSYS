8655b8f1de31ed1d7c9744b5ae28c45614e84070
daniel nava <dnavamoslercl@gmail.com>
2019-01-19 16:03:12 -0300

    otra prueba con cors

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ require('./config/config')
 const mongoose = require('mongoose')
 var express = require('express')
 var bodyParser = require('body-parser')
+var cors = require('cors');
+
+
 
 /* instancia de express */
 const app = express()
 
-// Add headers
-
+// use it before all route definitions
+app.use(cors({origin: 'https://water-sys-front.herokuapp.com/'}));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,24 +27,24 @@ app.use(bodyParser.json())
 /* importacion de rutas*/
 app.use( require('./routes/index'))
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://water-sys-front.herokuapp.com');
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'https://water-sys-front.herokuapp.com');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
-    next();
-});
+//     // Pass to next layer of middleware
+//     next();
+// });
 
 /* Conexion a base de datos */
 mongoose.connect(process.env.URL_DB, (err , res) => {
