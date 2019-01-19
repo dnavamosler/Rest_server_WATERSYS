2dd71f81179bcb0fd4d9468e34db40d06e9f675a
daniel nava <dnavamoslercl@gmail.com>
2019-01-19 15:55:16 -0300

    otra opcion

--- server.js
+++ server.js
@@ -8,12 +8,29 @@ require('./config/config')
 const mongoose = require('mongoose')
 var express = require('express')
 var bodyParser = require('body-parser')
-var cors = require('cors')
 
 /* instancia de express */
 const app = express()
 
-app.use(cors())
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://water-sys-front.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
